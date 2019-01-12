7d4a1ed5b45c636256e5d429dd3bb9228d3e1680
xgyuyu <409816503@qq.com>
2019-01-12 11:30:59 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers  } from 'redux'
+
+const reducer = ( state = [], action ) => {
+  return state
+}
+
+const store = createStore(reducer)
 
 ReactDOM.render(
     <div>Hello World123</div>, document.getElementById('app')
