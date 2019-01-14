41573599f40a25ca21e4ef951cde99353e05a649
xgyuyu <409816503@qq.com>
2019-01-14 20:57:42 +0800

    更新esling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,56 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": "airbnb",
-  "env": {
-    "browser": true,
-    "node": true
+  //文件名 .eslintrc.json
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    commonjs: true
   },
-  "parserOptions": {
-     "ecmaVersion": 6,
-     "sourceType": "module",
-     "ecmaFeatures": {
-       "jsx": true
-     }
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+      arrowFunctions: true,
+      classes: true,
+      modules: true,
+      defaultParams: true
+    },
+    sourceType: 'module'
   },
-  "globals": {
-    "window": true,
-    "document": true
-  },
-  "rules": {
-    "semi": [0],
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "no-trailing-spaces": 1, // 一行结束后面不要有空格
-    "linebreak-style": [0 ,"error", "windows"]
-  },
-  "plugins": [
-    "compat",
-    "import",
-    "promise"
-  ]
+  plugins: ['react'],
+  rules: {
+    'linebreak-style': ["off", "windows"],
+    "semi": ["error", "never"],
+    'no-empty': 0,
+    'comma-dangle': 0,
+    'no-unused-vars': 0,
+    'no-console': 0,
+    'no-const-assign': 2,
+    'no-dupe-class-members': 2,
+    'no-duplicate-case': 2,
+    'no-extra-parens': [2, 'functions'],
+    'no-self-compare': 2,
+    'accessor-pairs': 2,
+    'comma-spacing': [
+      2,
+      {
+        before: false,
+        after: true
+      }
+    ],
+    'constructor-super': 2,
+    'new-cap': [
+      2,
+      {
+        newIsCap: true,
+        capIsNew: false
+      }
+    ],
+    'new-parens': 2,
+    'no-array-constructor': 2,
+    'no-class-assign': 2,
+    'no-cond-assign': 2,
+    'no-mixed-spaces-and-tabs': 0
+  }
 }
