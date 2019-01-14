8bb982f550a1508f6c69ce72f919fae99b0d1ffa
wongbao <wong130551380@gmail.com>
2019-01-14 11:10:53 +0800

    添加新按钮操作

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,24 @@ import { increment, incrementAsync } from '../actions/counter'
 console.log(increment())
 
 export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.isOddAdd = this.isOddAdd.bind(this)
+  }
+
+  isOddAdd() {
+    if (this.props.counter % 2 === 0) {
+      this.props.increment()
+    } 
+  }
+
   render() {
     return (
       <div className="App">
         <p>我是测试的标题</p>
         {this.props.counter}
         <button onClick={this.props.increment}>+</button>
+        <button onClick={this.isOddAdd}>+Odd</button>
         <br />
         <button onClick={this.props.incrementAsync}>Async+</button>
       </div>
