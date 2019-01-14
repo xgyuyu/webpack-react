88ee16ec3303178ed0ffbc691c4dffcb8c59a2f3
wongbao <wong130551380@gmail.com>
2019-01-14 09:41:13 +0800

    delete unnecessary files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
-import users from './users'
 import counter from './counter'
 
 export default combineReducers({
-    users,
     counter
 })
--- src/reducers/users.js
+++
@@ -1,7 +0,0 @@
-const users = (state = {}, action = {}) => {
-    switch(action.type) {
-        default:
-        return state
-    }
-}
-export default  users
