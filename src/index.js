ccfa0ac33b51f2793b59c5a69381a05264c72add
xgyuyu <409816503@qq.com>
2019-01-14 11:17:10 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import './css/index.less'
 import './css/index.css'
 
-/* var a = 1
-console.log(a)
-function a() {
-  aaaaa
-} */
-
 ReactDOM.render(
   <div>Hello World123</div>, document.getElementById('app'),
 )
